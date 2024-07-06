b38b67327ac2fc58ed426f1ad0acf669d7c1d393
dikshagupta3094 <dikshagpragya@gmail.com>
2024-07-06 19:45:32 +0530

    make responsive

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -3,19 +3,19 @@ import { FaLinkedin } from "react-icons/fa";
 // import "./Team.css";
 function TeamCard(props) {
   return (
-    <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 w-[420px] figure h-[300px] cursor-pointer">
+    <figure class="flex flex-col md:flex-row bg-slate-100 rounded-xl px-0 py-8 md:p-0 w-[340px] md:w-[420px] h-[480px] md:h-[300px]">
       <img
         src={props.image}
-        className="m-auto w-[60%] h-full captianImg p-2 rounded-2xl"
+        className="m-auto w-[80%] md:w-[60%] h-full captianImg p-2 rounded-2xl"
       />
 
       <div className="flex ">
-        <figcaption className="flex flex-col justify-center items-center gap-y-3">
+        <figcaption className="flex flex-col justify-center items-center gap-y-3 w-[95%] text-center">
           <div className="font-bold text-xl">{props.name}</div>
           <div className="text-center">{props.place}</div>
-          <div className="cursor-pointer">
-            <FaLinkedin className="text-2xl" />
-          </div>
+          <a className="cursor-pointer" href={props.linkedin} target="_blank">
+            <FaLinkedin className="text-2xl text-blue-700" />
+          </a>
         </figcaption>
       </div>
     </figure>
